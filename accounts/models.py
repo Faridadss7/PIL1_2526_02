5b2f81d4149ef83884579e15cd3a5599ffafbfb0
Jean <jeanhoungue800@gmail.com>
2026-06-04 23:23:54 +0200

    Inscription connexion et deconnexion

--- accounts/models.py
+++ accounts/models.py
@@ -14,9 +14,14 @@ class User(AbstractUser):
     niveau = models.CharField(max_length=50, blank=True)
     bio = models.TextField(blank=True)
     photo = models.ImageField(upload_to='photos/', blank=True, null=True)
-    competences = models.TextField(blank=True)
-    lacunes = models.TextField(blank=True)
-    disponibilites = models.TextField(blank=True)
+    centre_interet = models.TextField(blank=True, null=True, db_column='Centre_interet')
+    date_inscription = models.DateTimeField(auto_now_add=True)
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = ['nom', 'prenom', 'telephone']
+    class Meta:
+        db_table = 'utilisateurs'
+
+    def __str__(self):
+        return f"{self.prenom} {self.nom}"
+    
    
